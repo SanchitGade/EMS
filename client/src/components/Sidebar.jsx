be5e11906ad3b3231edffcdcb2e2ac027357ac6b
SanchitGade <sanchitgade06@gmail.com>
2026-06-01 12:34:34 +0530

    Attendance Page UI Completed >_<

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -34,7 +34,7 @@ const SideBar = () => {
     window.location.href = "/login";    
   }
 
-  const role = "ADMIN" || "EMPLOYEE";
+  const role = "" || "EMPLOYEE";
 
   const navItems = [
     { name: "Dashboard", href: "/dashboard", icon: LayoutGridIcon },
